f654a5c33423707e93445c47760834edaa0dc182
Aurchon Datta <aurchond@gmail.com>
2023-07-25 13:00:22 -0400

    changed keyboard functionality to be in app.js

--- src/App.js
+++ src/App.js
@@ -18,11 +18,64 @@ function App() {
     coordYAttempt: 0,
   });
 
-  const onSelectLetter = () => {};
+  // function for selecting latter
+  const onSelectLetter = (keyValue) => {
+    // we're at the last letter space in the row, return
+    if (currAttemptState.coordXLetter > 4) return;
 
-  const onDelete = () => {};
+    // otherwise, get state of the board right now
+    const newBoardState = [...boardState];
 
-  const onEnter = () => {};
+    // update the key value (letter) of the state of the newBoard at the current position
+    newBoardState[currAttemptState.coordYAttempt][
+      currAttemptState.coordXLetter
+    ] = keyValue;
+
+    // use our predefined setBoard function to update the board to have this letter
+    setBoardState(newBoardState);
+
+    // set the current attempt state to whatever it was before but iterate the letter coordinate position by 1
+    setCurrAttemptState({
+      ...currAttemptState,
+      coordXLetter: currAttemptState.coordXLetter + 1,
+    });
+  };
+
+  // function for clicking delete
+  const onDelete = () => {
+    // if it's the first letter coordinate in the word, then can't delete it, return
+    if (currAttemptState.coordXLetter === 0) return;
+
+    // otherwise, make a new board state
+    const newBoardState = [...boardState];
+
+    // remove the key value (letter) of the state of the newBoard at the most recently
+    // placed letter position ( the current coordXLetter position is the blank space after it )
+    newBoardState[currAttemptState.coordYAttempt][
+      currAttemptState.coordXLetter - 1
+    ] = "";
+
+    // use our predefined setBoard function to update the board
+    setBoardState(newBoardState);
+
+    // set the current attempt state to whatever it was before but reduce the iterator's position by 1 because deletion
+    setCurrAttemptState({
+      ...currAttemptState,
+      coordXLetter: currAttemptState.coordXLetter - 1,
+    });
+  };
+
+  // function for clicking enter
+  const onEnter = () => {
+    // can only go to next attempt if put in 5 letters, return
+    if (currAttemptState.coordXLetter < 5) return;
+
+    // otherwise, we're now moving to the next attempt (next row) and the letter coordinate is 0 again
+    setCurrAttemptState({
+      coordYAttempt: currAttemptState.coordYAttempt + 1,
+      coordXLetter: 0,
+    });
+  };
 
   return (
     // reminder: className formats according to the related css in App.css
@@ -40,6 +93,9 @@ function App() {
           setBoardState,
           currAttemptState,
           setCurrAttemptState,
+          onSelectLetter,
+          onDelete,
+          onEnter,
         }}
       >
         <div className="game">
--- src/components/Key.js
+++ src/components/Key.js
@@ -2,44 +2,22 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyValue, isBigKey }) {
-  const { boardState, setBoardState, currAttemptState, setCurrAttemptState } =
-    useContext(AppContext);
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
 
   const selectLetter = () => {
     // pressed enter key
     if (keyValue == "ENTER") {
-      // can only go to next attempt if put in 5 letters, return
-      if (currAttemptState.coordXLetter < 5) return;
-
-      // otherwise, we're now moving to the next attempt (next row) and the letter coordinate is 0 again
-      setCurrAttemptState({
-        coordYAttempt: currAttemptState.coordYAttempt + 1,
-        coordXLetter: 0,
-      });
-    } else if (keyValue == "DELETE") {
+      onEnter();
     }
 
-    // pressed any other letter
+    // pressed delete key
+    else if (keyValue == "DELETE") {
+      onDelete();
+    }
+
+    // pressed any other key
     else {
-      // we're at the last letter space in the row, return
-      if (currAttemptState.coordXLetter > 4) return;
-
-      // otherwise, get state of the board right now
-      const newBoardState = [...boardState];
-
-      // update the key value (letter) of the state of the newBoard at the current position
-      newBoardState[currAttemptState.coordYAttempt][
-        currAttemptState.coordXLetter
-      ] = keyValue;
-
-      // use our predefined setBoard function to update the board to have this letter
-      setBoardState(newBoardState);
-
-      // set the current attempt state to whatever it was before but iterate the letter coordinate position by 1
-      setCurrAttemptState({
-        ...currAttemptState,
-        coordXLetter: currAttemptState.coordXLetter + 1,
-      });
+      onSelectLetter(keyValue);
     }
   };
 
